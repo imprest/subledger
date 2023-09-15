cfc35d1dcf1b29fba34b7548b01f130d3efba799
Hardik Varia <hardikvaria@gmail.com>
2023-09-15 15:47:09 +0000

    Tailwind should include correct files

--- assets/tailwind.config.cjs
+++ assets/tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   darkMode: ['class'],
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    '../lib/ghr_web/{components,controllers}/**/*.{heex,ex}'
+    '../lib/subledger_web/{components,controllers}/**/*.{heex,ex}'
   ],
   theme: {
     extend: {}
